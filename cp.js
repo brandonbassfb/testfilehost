50fb5966eea5baab52a16f2df47f0eed4f9dbd44
brandonbassfb <90646713+brandonbassfb@users.noreply.github.com>
2021-09-30 14:01:48 -0700

    Update cp.js

--- cp.js
+++ cp.js
@@ -1,4 +1,5 @@
 (function(window) {
+    console.log("facebook chat executing");
     var document = window.document;
     var pageId = window.fb_gtm_fbid
     var fbRoot = document.createElement('div');
